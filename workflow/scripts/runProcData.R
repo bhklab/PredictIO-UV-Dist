feb7c2f6f08fef6d288a5242bd4dd97183e9d82c
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2025-07-04 14:49:07 -0400

    add config step for processing data

--- workflow/scripts/runProcData.R
+++ workflow/scripts/runProcData.R
@@ -1,19 +1,35 @@
+# -----------------------------------------------------------
+# Processed Data Creation Script
+# This script reads input and output directory paths and study metadata
+# from `config_proc.yaml`, and performs the following:
+#
+#   - Loads raw MultiAssayExperiment (MAE) object from `data/rawdata`
+#   - Converts MAE to SummarizedExperiment (SE)
+#   - Loads gene signature data and metadata
+#   - Saves a combined processed data object to `data/procdata`
+#
+# Config file: config/config_proc.yaml
+# -----------------------------------------------------------
 ########################################################
 ## Load Libraries
 ########################################################
 library(MultiAssayExperiment)
 library(data.table)
 library(PredictioR)
+library(yaml)
 
-########################################################
-## Set Up Directories and Study Metadata
-########################################################
-dir_in <- "data/rawdata"
-dir_out <- "data/procdata"
+###########################################################
+## Set up working directory and study configuration
+###########################################################
+# Load configuration file
+config <- yaml::read_yaml("config/config_proc.yaml")
 
-study_icb <- "ICB_Gide"
-cancer_type <- "Melanoma"
-treatment_type <- "PD-(L)1"
+dir_in <- config$dir_in # "data/rawdata"
+dir_out <- config$dir_out # "data/procdata"
+
+study_icb <- config$study_icb # "ICB_Gide"
+cancer_type <- config$cancer_type # "Melanoma"
+treatment_type <- config$treatment_type # "PD-(L)1"  (Other options include: CTLA-4, IO+combo, etc.)
 
 ########################################################
 ## Load Raw ICB Dataset
