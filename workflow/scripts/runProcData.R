97848d72137ebbbcd9b5b42f6cc0066a62b82ce9
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2025-07-04 13:18:13 -0400

    update script to create process data

--- workflow/scripts/runProcData.R
+++ workflow/scripts/runProcData.R
@@ -1,43 +1,51 @@
-# load library
-
+########################################################
+## Load Libraries
+########################################################
 library(MultiAssayExperiment)
 library(data.table)
 library(PredictioR)
 
-####################################################
-# set up directory
-####################################################
-dir.in <- 'data/rawdata'
-dir.out <- 'data/procdata'
+########################################################
+## Set Up Directories and Study Metadata
+########################################################
+dir.in <- "data/rawdata"
+dir.out <- "data/procdata"
 
 study.icb <- "ICB_Gide"
 cancer_type <- "Melanoma"
 treatment <- "PD-(L)1"
 
-###################################################
-# load ICB data
-###################################################
-# download ICB Gide  dataset from 'https://www.orcestra.ca/clinical_icb/62f29e85be1b2e72a9c177f4'
-# locate at data/rawdata folder
+########################################################
+## Load Raw ICB Dataset
+########################################################
+# Ensure the following file is downloaded from:
+# https://www.orcestra.ca/clinical_icb/62f29e85be1b2e72a9c177f4
+# and placed in `data/rawdata/ICB_Gide.rds`
 
-dat <- readRDS(file = file.path(dir.in, 'ICB_Gide.rds')) # MAE object
-dat_icb <- createSE(dat) # create SE object: TPM expression matrix, clinical, and gene annotation data
+mae_obj <- readRDS(file.path(dir.in, "ICB_Gide.rds"))  # MultiAssayExperiment
+se_obj  <- createSE(mae_obj)  # Extract SummarizedExperiment (TPM + clinical)
 
-###########################################################
-## load signature data and signature meta-data information
-###########################################################
-
-load(file.path(dir.in, 'signature.rda'))
-load(file.path(dir.in, 'sig.info.rda'))
+########################################################
+## Load Gene Signature Data
+########################################################
+# Signature objects should be pre-curated and saved as .rda files
+load(file.path(dir.in, "signature.rda"))   # signature matrix
+load(file.path(dir.in, "sig.info.rda"))    # metadata for signatures
 
 dat <- list('ICB' = dat_icb,
             'signature' = signature,
             'sig.info' = sig.info)
 
-save(dat, file = file.path(dir.out, 
-                           paste(paste(study.icb, 
-                                       cancer_type, 
-                                       treatment, 
-                                       sep='__'), 
-                                       '.rda', sep="") ))_
+########################################################
+## Combine Data into a Named List and Save
+########################################################
+dat <- list(
+  ICB       = se_obj,
+  signature = signature,
+  sig.info  = sig.info
+)
 
+save(
+  dat, 
+  file = file.path(dir.out, paste0(paste(study.icb, cancer_type, treatment, sep = "__"), ".rda"))
+)
